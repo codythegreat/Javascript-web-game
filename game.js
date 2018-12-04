57171b9b87c703f72d86008fd3d8063a0ade40ea
Cody Maxie <38113810+codythegreat@users.noreply.github.com>
2018-12-04 09:32:28 -0600

    redid tabbing again for lower portion

--- game.js
+++ game.js
@@ -48,37 +48,37 @@ let progressBoard = document.getElementById('game-progress-text');
 let progressBoardRewardCounter = 0;
 let pointCounter = document.getElementById('point-counter');
 let rewards = [
-	['Tools', 25, 1.1, 0],
-	['Shack', 50, 1, 1],
-	['House', 100, 1, 3],
-	['Factory', 200, 2, 0],
-	['Laboratory', 350, 3.5, 3]
+  ['Tools', 25, 1.1, 0],
+  ['Shack', 50, 1, 1],
+  ['House', 100, 1, 3],
+  ['Factory', 200, 2, 0],
+  ['Laboratory', 350, 3.5, 3]
 ];
 
 const updatePoints = (pts) => {
-	points += pts;
-	pointCounter.textContent = `${points.toFixed(1)} Points`
+  points += pts;
+  pointCounter.textContent = `${points.toFixed(1)} Points`
 };
 
 const buildProgressBoardRow = () => {
-	let newRowElement = document.createElement('DIV');
-	newRowElement.className = "row";
-	newRowElement.id = `${progressBoardRewardCounter}th row`
-	progressBoard.appendChild(newRowElement);
-	progressBoard = newRowElement;
+  let newRowElement = document.createElement('DIV');
+  newRowElement.className = "row";
+  newRowElement.id = `${progressBoardRewardCounter}th row`
+  progressBoard.appendChild(newRowElement);
+  progressBoard = newRowElement;
 }
 
 const buildProgressBoardElement = (type) => {
-	progressBoard.textContent += ' ' + type + ' |';
+  progressBoard.textContent += ' ' + type + ' |';
 }
 
 const addReward = (reward) => {
-	if (points >= reward[1]) {
-		pointMultiplier *= reward[2];
-		cats += reward[3];
-		updatePoints(-reward[1]);
-		buildProgressBoardElement(reward[0]);
-	} 
+  if (points >= reward[1]) {
+    pointMultiplier *= reward[2];
+    cats += reward[3];
+    updatePoints(-reward[1]);
+    buildProgressBoardElement(reward[0]);
+  } 
 }
 
 document.getElementById("add-points").addEventListener("click", function(){ updatePoints(1 * cats * pointMultiplier); });
