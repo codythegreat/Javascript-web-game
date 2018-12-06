b7dfbb0068b8da8dcd7368c9334f1909849a3e3e
cody <maxiecody@gmail.com>
2018-12-05 21:37:48 -0600

    inventory and randome events added

--- game.js
+++ game.js
@@ -12,6 +12,159 @@ let climates = [
   ['Acid Rain', .3],
   ['Nuclear Winter', .1]
 ];
+let inventoryItems = [
+  ['ammo', 15],
+  ['leather', 15],
+  ['batteries', 20],
+  ['electronics', 25],
+  ['sheet metal', 5],
+  ['wood', 5],
+];
+let randomEvents = [
+  {
+    text: 'A survivor was shot in combat. -1 survivor',
+    scrapProdChange: 1,
+    foodProdChange: 1,
+    survivorChange: -1,
+    scrapChange: 0,
+    foodChange: 0,
+    diseaseChange: 0,
+    inventoryChange: null,
+    timer: 1,
+  },
+  {
+    text: 'A starving survivor was rescued. +1 survivor',
+    scrapProdChange: 1,
+    foodProdChange: 1,    
+    survivorChange: 1,
+    scrapChange: 0,
+    foodChange: 0,
+    diseaseChange: 0,
+    inventoryChange: null,
+    timer: 1,
+  },
+  {
+    text: 'A crate of food was discovered. +10 food',
+    scrapProdChange: 1,
+    foodProdChange: 1,
+    survivorChange: 0,
+    scrapChange: 0,
+    foodChange: 10,
+    diseaseChange: 0,
+    inventoryChange: null,
+    timer: 1,
+  },
+  {
+    text: 'A large crate of food was discovered. +25 food',
+    scrapProdChange: 1,
+    foodProdChange: 1,
+    survivorChange: 0,
+    scrapChange: 0,
+    foodChange: 25,
+    diseaseChange: 0,
+    inventoryChange: null,
+    timer: 1,
+  },
+  {
+    text: 'A stash of scrap was discovered. +10 scrap',
+    scrapProdChange: 1,
+    foodProdChange: 1,
+    survivorChange: 0,
+    scrapChange: 10,
+    foodChange: 0,
+    diseaseChange: 0,
+    inventoryChange: null,
+    timer: 1,
+  },
+  {
+    text: 'A large stash of scrap was discovered. +25 scrap',
+    scrapProdChange: 1,
+    foodProdChange: 1,
+    survivorChange: 0,
+    scrapChange: 25,
+    foodChange: 0,
+    diseaseChange: 0,
+    inventoryChange: null,
+    timer: 1,
+  },
+  {
+    text: 'Morale has been boosted. +50% daily scrap discovery',
+    scrapProdChange: 1.5,
+    foodProdChange: 1,
+    survivorChange: 0,
+    scrapChange: 0,
+    foodChange: 0,
+    diseaseChange: 0,
+    inventoryChange: null,
+    timer: 3,
+  },
+  {
+    text: 'Morale has been boosted. +50% daily food discovery',
+    scrapProdChange: 1,
+    foodProdChange: 1.5,
+    survivorChange: 0,
+    scrapChange: 0,
+    foodChange: 0,
+    diseaseChange: 0,
+    inventoryChange: null,
+    timer: 3,
+  },
+  {
+    text: 'The following item was scavenged: ',
+    scrapProdChange: 1,
+    foodProdChange: 1,
+    survivorChange: 0,
+    scrapChange: 0,
+    foodChange: 0,
+    diseaseChange: 0,
+    inventoryChange: 1,
+    timer: 1,
+  },
+  {
+    text: 'The following items were scavenged: ',
+    scrapProdChange: 1,
+    foodProdChange: 1,
+    survivorChange: 0,
+    scrapChange: 0,
+    foodChange: 0,
+    diseaseChange: 0,
+    inventoryChange: 2,
+    timer: 1,
+  },
+  {
+    text: 'The following items were scavenged: ',
+    scrapProdChange: 1,
+    foodProdChange: 1,
+    survivorChange: 0,
+    scrapChange: 0,
+    foodChange: 0,
+    diseaseChange: 0,
+    inventoryChange: 3,
+    timer: 1,
+  },
+  {
+    text: 'Your colony has contracted praire fever. -20% scrap and food production',
+    scrapProdChange: .8,
+    foodProdChange: .8,
+    survivorChange: 0,
+    scrapChange: 0,
+    foodChange: 0,
+    diseaseChange: 1,
+    inventoryChange: null,
+    timer: 3,
+  },
+  {
+    text: 'Your colony has contracted radiation sickness. -50% scrap and food production',
+    scrapProdChange: .5,
+    foodProdChange: .5,
+    survivorChange: 0,
+    scrapChange: 0,
+    foodChange: 0,
+    diseaseChange: 2,
+    inventoryChange: null,
+    timer: 3,
+  }
+];
 
 class Colony {
   constructor() {
@@ -22,6 +175,11 @@ class Colony {
     this.rewards = [];
     this.survivors = 1;
     this.foodPurchases = 0;
+    this.inventory = [];
+    this.tempScrapProd = 1;
+    this.tempFoodProd = 1;
+    this.disease = 0;
+    this.randomEventTimer = 0;
     // this.maximumScrap = 1000
   }
   updateScrap(scrp) {
@@ -82,6 +240,8 @@ class GameState {
     this.populationCounter = document.getElementById('colony-pop-text');
     this.colonyName = document.getElementById('colony-name-text');
     this.gameStatsDisplay = document.getElementById('game-stats-text');
+    this.randomEventDisplay = document.getElementById('random-event-text');
+    this.inventoryDisplay = document.getElementById('inventory-display');
   }
   updateScrapCounter() {
     this.scrapCounter.textContent = `${colony.scrap.toFixed()} Scrap`;
@@ -124,7 +284,7 @@ const completeAllBuyFoodRequirements = () => {
     game.updateFoodButton();
     game.updateScrapCounter();
 	}
-}
+} 
 
 const initColonyWithName = () => {
   colonyName = document.getElementById('colony-name-input').value;
@@ -151,15 +311,35 @@ const gameOverIfZeroFood = () => {
 	}
 }
 
+const gameOverIfZeroSurvivors = () => {
+  if (colony.survivors == 0) {
+    alert('Your survivors have perished. Play again, you may get further!');
+    document.getElementById('add-points').style.visibility = 'hidden';
+  }
+}
+
+const scrapAmountAllVariables = () => {
+  return colony.survivors * colony.scrapMultiplier * colony.tempScrapProd * game.climate[1];
+}
+
 const advanceOneGameDay = () => {
+  if (colony.randomEventTimer > 0) {colony.randomEventTimer--;}
+  else {
+    colony.tempScrapProd = 1;
+    colony.tempFoodProd = 1;
+    colony.disease = 0;
+    game.randomEventDisplay.textContent = '';
+  }
   if (colony.name != '') {
-  colony.updateScrap(1 * colony.survivors * colony.scrapMultiplier * game.climate[1]);
+  colony.updateScrap(scrapAmountAllVariables());
   colony.consume();
   game.incrementDay();
   game.updateScrapCounter();
   game.updateFoodCounter();
+  game.updatePopCounter();
   game.updateGameStats();
   gameOverIfZeroFood();
+  gameOverIfZeroSurvivors();
   } else {alert('Please name your colony.');}
 }
 
@@ -172,6 +352,46 @@ const pointButtonLoading = () => {
   }, 2000);
 }
 
+const addElementForInventoryItem = (item) => {
+  let btn = document.createElement('button');
+  let btnTextNode = document.createTextNode(`${item[0]} | sell for ${item[1]} scrap`);
+  btn.appendChild(btnTextNode);
+  game.inventoryDisplay.appendChild(btn);
+  btn.onclick = function() {
+    colony.scrap += item[1];
+    btn.parentNode.removeChild(btn);
+    game.updateScrapCounter();
+  }
+}
+
+const addRandomEvent = () => {
+  let randomEvent = randomEvents[Math.floor(Math.random()*randomEvents.length)];
+  colony.tempScrapProd = randomEvent.scrapProdChange;
+  colony.tempFoodProd = randomEvent.foodProdChange;
+  colony.survivors += randomEvent.survivorChange;
+  colony.scrap += randomEvent.scrapChange;
+  colony.food += randomEvent.foodChange;
+  colony.disease = randomEvent.diseaseChange;
+  colony.randomEventTimer = randomEvent.timer;
+  let inventoryItemsAdded = '';
+  let item = [];
+  for (let i = 0; i < randomEvent.inventoryChange; i++) {
+    item = inventoryItems[Math.floor(Math.random()*inventoryItems.length)];
+    colony.inventory += item;
+    inventoryItemsAdded += item[0] + ', ';
+    addElementForInventoryItem(item);
+  }
+  game.randomEventDisplay.textContent = randomEvent.text + inventoryItemsAdded.substring(0, inventoryItemsAdded.length - 2);
+}
+
+const chanceOfRandomEventIfTimerZero = () => {
+  if (colony.randomEventTimer == 0) {
+    if (Math.ceil(Math.random()*5) == 5) {
+      addRandomEvent();
+    }
+  }
+}
+
 document.getElementById('buy-food').addEventListener("click", function(){ 
   completeAllBuyFoodRequirements();
 });
@@ -182,7 +402,8 @@ document.getElementById("add-colony-name").addEventListener("click", function(){
   document.getElementById("colony-name-input").style.visibility = "hidden";
 });
 
-document.getElementById("add-points").addEventListener("click", function(){ 
+document.getElementById("add-points").addEventListener("click", function(){
+  chanceOfRandomEventIfTimerZero(); 
   pointButtonLoading();
   setTimeout(function() {
     advanceOneGameDay();
